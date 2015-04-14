522f72784076081422edcb80dc214f9e426b8854
cheshaneko <panykeywr@gmail.com>
2015-04-14 10:19:38 -0400

    add "api" into resource path

--- web/streamer.js
+++ web/streamer.js
@@ -1,7 +1,7 @@
 var streamApp = angular.module('streamerApp', ['ngResource']);
 streamApp.controller('StreamerController', function($scope, $http) {
     var loadData = function() {
-        $http.get('streams').
+        $http.get('api/streams').
             success(function(data) {
                 $scope.streams = data.streams;    
             });
@@ -9,7 +9,7 @@ streamApp.controller('StreamerController', function($scope, $http) {
     loadData();
     $scope.addChannel = function() {
         $scope.channel.process_status = 'started';
-        $http.post('streams', {channel: $scope.channel}).
+        $http.post('api/streams', {channel: $scope.channel}).
             success(function(data) {
                 $scope.streams.push(data.stream);
                 $scope.channel = '';
@@ -17,21 +17,21 @@ streamApp.controller('StreamerController', function($scope, $http) {
     };
     $scope.remove = function(stream) {
         if ($scope.streams.indexOf(stream) !== -1) {
-            $http.delete('streams/' + stream.channel).
+            $http.delete('api/streams/' + stream.channel).
                 success(function(data) {
                     $scope.streams.splice($scope.streams.indexOf(stream), 1);
                 });
         }
     }
     $scope.stop = function(stream) {
-        $http.put('streams/' + stream.channel, {process_status: 'stopped'}).
+        $http.put('api/streams/' + stream.channel, {process_status: 'stopped'}).
             success(function(data) {
                 stream['process_status'] = data.stream['process_status'];
                 stream['channel_status'] = data.stream['channel_status'];
             });
     }
     $scope.start = function(stream) {
-        $http.put('streams/' + stream.channel, {process_status: 'started'}).
+        $http.put('api/streams/' + stream.channel, {process_status: 'started'}).
             success(function(data) {
                 stream['process_status'] = data.stream['process_status'];
                 stream['channel_status'] = data.stream['channel_status'];
